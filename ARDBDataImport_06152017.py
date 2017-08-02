871833e542b180b8ab6a2cd26eddb4698624e6d5
Changchuan Yin <14200582+cyinbox@users.noreply.github.com>
2017-08-02 16:12:06 -0500

    Updated with new library name
    Updated with new library name

--- ARDBDataImport_06152017.py
+++ ARDBDataImport_06152017.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-# Program detecting antibiotic resistance genes
+# Program for constructing antibiotic resistance genes
 #
 # Changchuan Yin, Ph.D.
 # Dept. of Mathematics, Statistics and Computer Science
@@ -12,14 +12,15 @@
 # Last update 06/16/2017
 #
 # Citation
-# Yin, C. (2017) GeneBow: a pipeline for detecting antibiotic resistance genes.
+# Yin, C. (2017). GeneBow: a pipeline for detecting antibiotic resistance genes.
+# University of Illinois at Chicago
 #
 '''
 # Program to construct ARG sequences 
 # Inputs:ar_genes.tab contains ARG protein accessId,resistance_profile.tab is for resistance profile
 # output: ar_genes_cds.fasta: ARG genes and their DNA sequences
 import pandas as pd
-import GenePod as gene
+import LibGeneBow as gene
 
 fastaFileName='./ARGData/ar_genes_cds.fasta'
 handle = open(fastaFileName, 'w')
@@ -48,8 +49,8 @@ for row in range(1,numRow):
 df = pd.read_csv('./ARGData/ar_genes.tab',sep='\t',header=None)
 numRow=df[0].count()
 
-print(df[0][2])
-print(df[0].count())#row 
+#print(df[0][2])
+#print(df[0].count())#row 
 numRow=df[0].count()
 
 proteinIds=[]
@@ -63,50 +64,68 @@ for row in range(0,numRow):
   proteinIds.append(proteinId)
   IdProfiles[proteinId]=profile #Store id and profile relationship
 
-''' For dev test
-print('Test2:',proteinIds)
-print('IdProfile',IdProfiles)
+# For dev only
+#print('Test1:',proteinIds)
+#print('Test2:',IdProfiles) #ProteinId and its antibiotic resistances
+'''
+proteinId='YP_637442'
+profileNameByProteinId=IdProfiles[proteinId]
+print('Test3:',profileNameByProteinId)
+antibioticProfileByProfileId=antibioticProfiles[profileNameByProteinId]
+print('Test4:',antibioticProfileByProfileId)
+#print('proteinIds:',proteinIds)
+resistances=', '.join(antibioticProfileByProfileId)
+resistances='('+resistances+')'
+print ('antibiotic resistanceX:',resistances)
+
 proteinId='AAC44793'
 profileNameByProteinId=IdProfiles[proteinId]
-print('Test2',profileNameByProteinId)
+print('Test5:',profileNameByProteinId)
 antibioticProfileByProfileId=antibioticProfiles[profileNameByProteinId]
-print('Test3',antibioticProfileByProfileId)
-print('proteinIds:',proteinIds)
+print('Test6:',antibioticProfileByProfileId)
+#print('proteinIds:',proteinIds)
+resistances=', '.join(antibioticProfileByProfileId)
+resistances='('+resistances+')'
+print ('antibiotic resistanceY:',resistances)
 '''
-#proteinIds = ['ABS70977','CAD38267','BAB71949XX','CAG44462']#,'AAS64386','ABS70977','CAD38267','NP_334681','YP_905232','ABR04609','ABL89492','ABG06386'];
 
+#proteinIds = ['YP_637442','AAC44793','AAA03550']#,'ABS70977','CAD38267']
 cnt=0
-for proteinAccessId in proteinIds:
+for proteinId in proteinIds:
  try:
-  print('\n'+'starting to process protein:',proteinAccessId)
+  print('\n'+'starting to process protein:',proteinId)
  
-  [genBankId,cdsDirection,startPos,endPos,desc] = gene.getGenBankByProteinId(proteinAccessId)
-  profileNameByProteinId=IdProfiles[proteinAccessId]
+  [genBankId,cdsDirection,startPos,endPos,desc] = gene.getGenBankByProteinId(proteinId)
+  profileNameByProteinId=IdProfiles[proteinId]
   antibioticProfileByProfileId=antibioticProfiles[profileNameByProteinId]
   #print('antibioticProfile:',antibioticProfileByProfileId)
   
-  print('protein access Id:',proteinAccessId)
+  print('protein access Id:',proteinId)
   print('gene access Id:',genBankId)
   print('cds direction:',cdsDirection)
   print('cds starts:',startPos)
   print('cds ends:',endPos)
   print('descs:',desc)
-  print('antibiotic resistance:',antibioticProfileByProfileId[0])
+  #print('antibiotic resistance:',antibioticProfileByProfileId[0])
+  resistances=', '.join(antibioticProfileByProfileId)
+  resistances='('+resistances+')'
+  print ('antibiotic resistance:',resistances)
+
  
   cds=gene.getGenBankCDS(genBankId,cdsDirection,startPos,endPos) 
-  header=proteinAccessId+'|'+genBankId+'|'+str(cdsDirection)+'|'+str(startPos)+'|'+str(endPos)+'|'+desc+'|'+antibioticProfileByProfileId[0]
+  header=proteinId+'|'+genBankId+'|'+str(cdsDirection)+'|'+str(startPos)+'|'+str(endPos)+'|'+desc+'|'+resistances
   print('Header:',header)
-  print('seq:',cds)
+  #print('seq:',cds)
   gene.writeFastaFileOneRecord(header,cds,handle)
   cnt=cnt+1
- 
+  print('cnt=',cnt)
  except Exception as err:
     print('Error:',err)
     continue  
 
-handle.close()
 print('Number of ARGs:',cnt)
-print('All protein Ids have been processed.') 
+print('All ARG CDS have been processed.') 
+
 
 
  
